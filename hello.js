4a501f2999f3d05b1f38c246b1942200332e33bf
Nataly Denisyuk <denisyuk.natalka@gmail.com>
2022-12-05 21:23:19 +0300

    nata_test

---
+++ hello.js
@@ -0,0 +1,8 @@
+var admin;
+var name1;
+name1 = "Jone";
+admin = name1;
+//console.log("Hello , ".concat(admin, "!"));
+alert("Hello , ".concat(admin, "!"));
+var age = prompt("Haw old are you?", 100);
+alert("You are ".concat(age), "!");
---
+++ hello.ts
@@ -0,0 +1,9 @@
+let admin;
+let name1;
+let age;
+age = prompt("Сколько тебе лет?", "100");
+name1 = "Jone";
+admin = name1;
+console.log(`Hello , ${admin}!`);
+
+alert(`Тебе ${age} лет!`);
